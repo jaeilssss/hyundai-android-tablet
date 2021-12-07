d1174d18109ebb2ac236c3c219cd8cb2bb2e6277
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-12-07 16:38:19 +0900

    리사이클로뷰 어댑터 코드 및 FavoriteData 모델 구현

---
+++ app/src/main/java/com/obigo/hkmotors/adapter/MyFavoriteRecyclerAdapter.java
@@ -0,0 +1,61 @@
+package com.obigo.hkmotors.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.obigo.hkmotors.R;
+import com.obigo.hkmotors.model.FavoriteData;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class MyFavoriteRecyclerAdapter extends RecyclerView.Adapter<MyFavoriteRecyclerAdapter.MyFavoriteRecyclerViewHolder> {
+
+    public ArrayList<FavoriteData> list = new ArrayList<>();
+
+    public MyFavoriteRecyclerAdapter(ArrayList<FavoriteData> list) {
+        this.list = list;
+    }
+
+    @Override
+    public MyFavoriteRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater layoutInflater =  (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = layoutInflater.inflate(R.layout.my_favorite_item,parent,false);
+        return new MyFavoriteRecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyFavoriteRecyclerViewHolder holder, int position) {
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+    public class MyFavoriteRecyclerViewHolder extends RecyclerView.ViewHolder{
+
+        TextView title ;
+        TextView date;
+        MyFavoriteRecyclerViewHolder(View itemView)
+        {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.my_favorite_item_title);
+            date = itemView.findViewById(R.id.my_favorite_item_date);
+        }
+
+        public void setData(String title , String date){
+            this.title.setText(title);
+            this.date.setText(date);
+        }
+    }
+}
---
+++ app/src/main/java/com/obigo/hkmotors/model/FavoriteData.java
@@ -0,0 +1,22 @@
+package com.obigo.hkmotors.model;
+
+public class FavoriteData {
+    String title;
+    String date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
